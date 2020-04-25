7da5b189dfa9bec139951c4243442dbcf94af0d5
Anwesh43 <anweshthecool0@gmail.com>
2020-04-26 04:02:05 +0530

    adding BoomerangHalfCircle class

--- index.ts
+++ index.ts
@@ -192,3 +192,27 @@ class BHCNode {
         return this
     }
 }
+
+class BoomerangHalfCircle {
+
+    root : BHCNode = new BHCNode(0)
+    curr : BHCNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
