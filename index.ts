804f39277e5579127e1c3e719dba7cdb409a2612
Anwesh43 <anweshthecool0@gmail.com>
2020-04-26 03:37:44 +0530

    adding Animator classa

--- index.ts
+++ index.ts
@@ -38,3 +38,24 @@ class Stage {
         stage.handleTap()
     }
 }
+
+
+class Animator {
+
+    animated : boolean = false
+    interval : number
+
+    start(cb : Function) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(cb, delay)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
